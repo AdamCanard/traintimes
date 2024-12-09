7444ff59377e4e25f054adf4da80519a66b64a38
adamcanard <azcunard@gmail.com>
2024-12-09 11:05:50 -0700

    user can select red or blue line, the direction and their initial train, also can select edning train. needs wait screen and post data still

--- src/app/components/trainlist.tsx
+++ src/app/components/trainlist.tsx
@@ -1,11 +1,13 @@
 "use client";
-import { useContext, useEffect, useState } from "react";
+import { FormEvent, useContext, useEffect, useState } from "react";
 import { TopLevelContext } from "../context/toplevelcontext";
 import TrainLine from "./trainline";
 
 export default function TrainList() {
   const [trainList, setTrainList] = useState([]);
-  const { active } = useContext(TopLevelContext);
+  const [reverseTrainList, setReverseTrainList] = useState<string[]>([]);
+  const [startTrain, setStartTrain] = useState("");
+  const { active, direction, setDisable } = useContext(TopLevelContext);
 
   //API expectations
   //passes red or blue via params
@@ -15,6 +17,8 @@ export default function TrainList() {
       const response = await fetch("/api/train/" + active, { method: "GET" });
       const data = await response.json();
       setTrainList(data.trains);
+      const reverseList: string[] = [...data.trains];
+      setReverseTrainList(reverseList.reverse());
     } catch (err: unknown) {
       if (err instanceof Error) {
         return new Response(
@@ -32,13 +36,87 @@ export default function TrainList() {
 
   useEffect(() => {
     getTrains();
-  }, []);
+  }, [active]);
+
+  const handleChoice = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const element = e.target as HTMLInputElement;
+    setStartTrain(element.value);
+    setDisable(true);
+  };
+
+  const getIndex = (startTrain: string, trainList: string[]) => {
+    for (let i = 0; i < trainList.length; i++) {
+      if (trainList[i] == startTrain) {
+        return i + 1;
+      }
+    }
+  };
 
   return (
-    <>
-      {trainList.map((train, index) => {
-        return <TrainLine train={train} key={index} />;
-      })}
-    </>
+    <div className={"pt-2"}>
+      {startTrain === "" ? (
+        <>
+          <DirectionDecider />
+          <form className={"w-full px-4"} onChange={(e) => handleChoice(e)}>
+            <select className={"w-full h-12 text-lg"}>
+              {direction === 0 ? (
+                <TrainLine trains={trainList} />
+              ) : (
+                <TrainLine trains={reverseTrainList} />
+              )}
+              )
+            </select>
+          </form>
+        </>
+      ) : (
+        <>
+          <div className={"pl-4 text-3xl text-white font-bold"}>
+            {" "}
+            Select End Train
+          </div>
+          <form className={"w-full px-4"} onChange={(e) => handleChoice(e)}>
+            <select className={"w-full h-12 text-lg"}>
+              {direction === 0 ? (
+                <TrainLine
+                  trains={trainList.slice(getIndex(startTrain, trainList))}
+                />
+              ) : (
+                <TrainLine
+                  trains={reverseTrainList.slice(
+                    getIndex(startTrain, reverseTrainList),
+                  )}
+                />
+              )}
+              )
+            </select>
+          </form>
+        </>
+      )}
+    </div>
+  );
+}
+
+export function DirectionDecider() {
+  const { active, direction, setDirection } = useContext(TopLevelContext);
+  return (
+    <div className={"w-full h-16 flex justify-around flex-row items-center"}>
+      <div
+        id={direction === 0 ? "ButtonActive" : "Button"}
+        onClick={() => {
+          setDirection(0);
+        }}
+      >
+        {active === "red" ? "Tuscany" : "69 St Station"}
+      </div>
+      <div
+        id={direction === 1 ? "ButtonActive" : "Button"}
+        onClick={() => {
+          setDirection(1);
+        }}
+      >
+        {active === "blue" ? "Saddletowne" : "Somerset Bridlewood"}
+      </div>
+    </div>
   );
 }
