61034756d69c0f1065fcc729cf2cd92af0bd9034
adamcanard <azcunard@gmail.com>
2024-12-09 11:04:33 -0700

    added disable clause on header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,29 +3,44 @@ import { useContext } from "react";
 import { TopLevelContext } from "../context/toplevelcontext";
 
 export default function Header() {
-  const { active, setActive } = useContext(TopLevelContext);
+  const { active, setActive, disable } = useContext(TopLevelContext);
   return (
-    <header
-      className={
-        "w-full bg-[#24AE51] h-16 flex justify-around flex-row items-center"
-      }
-    >
-      <div
-        id={active === "red" ? "RedLineButtonActive" : "RedLineButton"}
-        onClick={() => {
-          setActive("red");
-        }}
-      >
-        REDLINE
-      </div>
-      <div
-        id={active === "blue" ? "BlueLineButtonActive" : "BlueLineButton"}
-        onClick={() => {
-          setActive("blue");
-        }}
-      >
-        BLUELINE
-      </div>
-    </header>
+    <>
+      {disable ? (
+        <>
+          {" "}
+          <header
+            className={
+              "w-full bg-[#24AE51] h-16 flex justify-around flex-row items-center"
+            }
+          ></header>
+        </>
+      ) : (
+        <>
+          <header
+            className={
+              "w-full bg-[#24AE51] h-16 flex justify-around flex-row items-center"
+            }
+          >
+            <div
+              id={active === "red" ? "RedLineButtonActive" : "RedLineButton"}
+              onClick={() => {
+                setActive("red");
+              }}
+            >
+              REDLINE
+            </div>
+            <div
+              id={active === "blue" ? "BlueLineButtonActive" : "BlueLineButton"}
+              onClick={() => {
+                setActive("blue");
+              }}
+            >
+              BLUELINE
+            </div>
+          </header>
+        </>
+      )}
+    </>
   );
 }
