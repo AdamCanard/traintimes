2b65b599ca461d1290633047bb8982f4f8669f48
adamcanard <azcunard@gmail.com>
2024-12-09 13:00:36 -0700

    added waitng screen function and post function

--- src/app/components/waiting.tsx
+++ src/app/components/waiting.tsx
@@ -1,3 +1,57 @@
+import { useContext } from "react";
+import { TopLevelContext } from "../context/toplevelcontext";
+
 export default function Waiting() {
-  return <></>;
+  const { riding, setEndTime, startTime, endTime } =
+    useContext(TopLevelContext);
+
+  const postData = async () => {
+    const formData = new FormData();
+    formData.append("start", riding.split(";")[1]);
+    formData.append("end", riding.split(":")[2]);
+    formData.append("difference", endTime - startTime + "");
+    try {
+      const response = await fetch("/api/time", {
+        method: "POST",
+        body: formData,
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        return new Response(
+          JSON.stringify({ error: err.message || err.toString() }),
+          {
+            status: 500,
+            headers: {},
+          },
+        );
+      } else {
+        console.log(err);
+      }
+    }
+  };
+  const handleClick = () => {
+    setEndTime(Date.now() / 1000);
+    postData();
+  };
+
+  return (
+    <div className={"flex items-center gap-4 flex-col"}>
+      <div className={"px-8 text-white font-bold text-3xl text-center"}>
+        Riding the {riding.split(":")[0]} Line from {riding.split(":")[1]} to{" "}
+        {riding.split(":")[2]}
+      </div>
+      <div>
+        <div className={"text-white text-md"}>Press me when you get off!</div>
+        <div
+          id="Button"
+          className={"text-lg text-white text-center font-bold"}
+          onClick={handleClick}
+        >
+          {riding.split(":")[2]}
+        </div>
+      </div>
+    </div>
+  );
 }
